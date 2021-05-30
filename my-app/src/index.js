25c90cd105ecd75b96a136ee17292db4065cf281
GyeongwonYun <yun@claire.team>
2021-05-30 16:14:45 +0900

    ui change V1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import ReactPlayer from "react-player";
+import { Scrollbars } from "react-custom-scrollbars-2";
+
 import { getHighlightTimeStamp } from "./210522_highlights";
 import "./index.css";
 class Video extends Component {
@@ -50,67 +52,67 @@ class Video extends Component {
     const inner = this.getWindowDimensions();
 
     return (
-      <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start" }}>
-        <div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}>
-          <div class="playerContainer">
-            <ReactPlayer
-              ref={this.ref}
-              className="react-player fixed-bottom"
-              url={this.props.url}
-              width="100%"
-              height="100%"
-              controls={true}
-              onKeyDown={this.handleKeyPress}
-            />
+      <Scrollbars style={{ width: "97vw", height: "97vh", marginBottom: "3vh" }}>
+        <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start" }}>
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}>
+            <div class="playerContainer">
+              <ReactPlayer
+                ref={this.ref}
+                className="react-player fixed-bottom"
+                url={this.props.url}
+                width="100%"
+                height="100%"
+                controls={true}
+                onKeyDown={this.handleKeyPress}
+              />
+            </div>
           </div>
-        </div>
-        <div class="sideBar">
-          <div class="sideBarTitle">
-            <text style={{ fontSize: "32px" }}>camera</text>
-          </div>
-          <div class="selectButtons">
-            <button class="cameraButton" onClick={() => window.scrollTo(0, 0)}>
-              left
-            </button>
-            <button class="cameraButton" onClick={() => window.scrollTo(0, inner.height)}>
-              main
-            </button>
-            <button class="cameraButton" onClick={() => window.scrollTo(0, inner.height * 2)}>
-              right
-            </button>
-          </div>
-          <div
-            class="tags"
-            style={{
-              width: Math.min(720, inner.Width - 32),
-            }}
-          >
-            {hl.map((val) => {
-              console.log(val);
-              return (
-                <button
-                  class="button"
-                  onClick={() => this.player.seekTo(val.min * 60 + val.sec)}
-                  style={{
-                    display: "flex",
-                    fontSize: 16,
-                    alignText: "left",
-                    marginTop: 8,
-                    backgroundColor: "white",
-                    borderStyle: "solid",
-                    borderColor: "gray",
-                    borderWidth: 2,
-                    borderRadius: 16,
-                    padding: 12,
-                  }}
-                >
-                  {val.min} 분 {val.sec} 초: {"\r" + val.tag}
+
+          <div class="sideBar">
+            <div class="sideBarTop">
+              <div class="sideBarTitle">
+                <text style={{ fontSize: "32px" }}>camera</text>
+              </div>
+              <div class="selectButtons">
+                <button class="cameraButton" onClick={() => window.scrollTo(0, 0)}>
+                  left
                 </button>
-              );
-            })}
+                <button class="cameraButton" onClick={() => window.scrollTo(0, inner.height)}>
+                  main
+                </button>
+                <button class="cameraButton" onClick={() => window.scrollTo(0, inner.height * 2)}>
+                  right
+                </button>
+              </div>
+            </div>
+            <Scrollbars class="tags">
+              {hl.map((val) => {
+                console.log(val);
+                return (
+                  <button
+                    class="oneTag"
+                    onClick={() => this.player.seekTo(val.min * 60 + val.sec)}
+                    style={{
+                      display: "flex",
+                      fontSize: 16,
+                      alignText: "left",
+                      marginTop: 8,
+                      backgroundColor: "white",
+                      borderStyle: "solid",
+                      borderColor: "gray",
+                      borderWidth: 2,
+                      borderRadius: 16,
+                      padding: 12,
+                    }}
+                  >
+                    {val.min} 분 {val.sec} 초: {"\r" + val.tag}
+                  </button>
+                );
+              })}
+            </Scrollbars>
           </div>
         </div>
-      </div>
+      </Scrollbars>
     );
   }
 }
