4979f3b5c56d81d63ff9f3232eba3b5350dc011d
Gyeongwon Yun <gyeongwonyun@gmail.com>
2021-06-24 01:48:54 +0900

    route changed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,41 +4,30 @@ import { BrowserRouter as Router, Route, Link, Switch, useParams } from "react-r
 import "./index.css";
 import Video from "./player";
 
-window.addEventListener("scroll", function () {
-  console.log(window.pageYOffset + "px");
-  //console.log(window.innerHeight  + 'px')
-  if (window.pageYOffset < window.innerHeight * 0.7) window.scrollTo(0, 0);
-  else if (window.pageYOffset < window.innerHeight * 1.4) window.scrollTo(0, window.innerHeight);
-  else window.scrollTo(0, window.innerHeight * 2);
-});
+// window.addEventListener("scroll", function () {
+//   console.log(window.pageYOffset + "px");
+//   //console.log(window.innerHeight  + 'px')
+//   if (window.pageYOffset < window.innerHeight * 0.7) window.scrollTo(0, 0);
+//   else if (window.pageYOffset < window.innerHeight * 1.4) window.scrollTo(0, window.innerHeight);
+//   else window.scrollTo(0, window.innerHeight * 2);
+// });
 
-const ReactPlayerUrl = () => {
+function ReactPlayerUrl() {
   let { timeStamp } = useParams();
   console.log(timeStamp);
   const newUrl = "videos/210530_left.MP4#t=" + timeStamp;
   console.log(newUrl);
   return <Video url={newUrl} playerName="leftCamera" isWIP={false} />;
-};
+}
 
 ReactDOM.render(
   <div style={{ overflow: "hidden" }}>
     <Router>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/t/3">time</Link>
-        </li>
-        <li>
-          <Link to="/users">users</Link>
-        </li>
-      </ul>
       <Switch>
         <Route path="/home">
           <Video url="videos/210530_left.MP4" playerName="leftCamera" isWIP={false} />
         </Route>
-        <Route path="/t/:timeStamp" children={<ReactPlayerUrl />} />
+        <Route path="/:timeStamp" children={<ReactPlayerUrl />} />
         <Route path="/users">
           <div>something</div>
         </Route>
